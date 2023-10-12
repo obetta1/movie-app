6377c3155da09be7130241347e0ba3581e7af8c6
obetta1 <obettabenjamin44@gmail.com>
2023-10-12 07:18:33 +0100

    theme fix

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,55 +1,30 @@
 import 'package:flutter/material.dart';
 
-ThemeData themeData(){
+ThemeData themeData() {
   return ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-    fontFamily: 'Avenir',
-    textTheme: textTheme()
-  );
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: 'Avenir',
+      textTheme: textTheme());
 }
 
-TextTheme textTheme(){
+TextTheme textTheme() {
   return const TextTheme(
-    displayLarge: TextStyle(
-      color: Colors.black,
-      fontSize: 70,
-      fontWeight: FontWeight.bold
-    ),
+      displayLarge: TextStyle(
+          color: Colors.black, fontSize: 70, fontWeight: FontWeight.bold),
       displayMedium: TextStyle(
-          color: Colors.black,
-          fontSize: 32,
-          fontWeight: FontWeight.bold
-      ),
+          color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold),
       displaySmall: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.bold
-      ),
+          color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
       headlineLarge: TextStyle(
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.normal
-      ),
+          color: Colors.black, fontSize: 20, fontWeight: FontWeight.normal),
       headlineMedium: TextStyle(
-          color: Colors.grey,
-          fontSize: 20,
-          fontWeight: FontWeight.normal
-      ),
+          color: Colors.grey, fontSize: 20, fontWeight: FontWeight.normal),
       headlineSmall: TextStyle(
-          color: Colors.black,
-          fontSize: 12,
-          fontWeight: FontWeight.bold
-      ),
+          color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold
+          //
+          ),
       bodyMedium: TextStyle(
-          color: Colors.black,
-          fontSize: 12,
-          fontWeight: FontWeight.bold
-      ),
+          color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold),
       bodySmall: TextStyle(
-          color: Colors.black,
-          fontSize: 10,
-          fontWeight: FontWeight.bold
-      )
-
- );
+          color: Colors.black, fontSize: 10, fontWeight: FontWeight.bold));
 }
